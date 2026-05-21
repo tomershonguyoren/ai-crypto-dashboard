0e179db5d5a097024a1d88357bd0b9c37f7e83ad
Deployment Agent <deploy@example.com>
2026-05-21 13:30:50 +0300

    feat: add dynamic randomized memes to MemeWidgetComponent

--- frontend/src/app/components/dashboard/widgets/meme-widget/meme-widget.component.ts
+++ frontend/src/app/components/dashboard/widgets/meme-widget/meme-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,11 +8,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './meme-widget.component.html',
   styleUrls: ['./meme-widget.component.scss']
 })
-export class MemeWidgetComponent {
+export class MemeWidgetComponent implements OnInit {
   @Input() data: string = '';
   @Output() onVote = new EventEmitter<{ widgetType: string, isPositive: boolean }>();
 
   hasVoted = false;
+  currentMemeUrl: string = '';
+
+  private readonly memeUrls: string[] = [
+    'https://i.imgflip.com/5c7lwq.jpg',
+    'https://i.imgflip.com/1ur9b0.jpg',
+    'https://i.imgflip.com/4tztcw.jpg',
+    'https://i.imgflip.com/2s3x2p.jpg',
+    'https://i.imgflip.com/4i9q9o.jpg'
+  ];
+
+  ngOnInit(): void {
+    this.selectRandomMeme();
+  }
+
+  selectRandomMeme(): void {
+    const randomIndex = Math.floor(Math.random() * this.memeUrls.length);
+    this.currentMemeUrl = this.memeUrls[randomIndex];
+  }
 
   vote(isPositive: boolean) {
     if (this.hasVoted) return;
@@ -20,3 +38,4 @@ export class MemeWidgetComponent {
     this.onVote.emit({ widgetType: 'fun_meme', isPositive });
   }
 }
+
